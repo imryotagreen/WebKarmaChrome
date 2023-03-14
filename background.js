4c9657544238d12288ef4d54513f0c9207a3c75e
Nicolas Perrier <nicolas.perrier@etu.unilim.fr>
2023-03-14 15:39:30 +0100

    main : adding document event

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let btn = document.getElementById('login-btn');
+    btn.addEventListener('click', function() {
+        chrome.tabs.create({ url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" }, function(tab) {
+            chrome.tabs.executeScript(tab.id, { code: "alert(document.title);" });
+        });
+    });
+});
+
 chrome.runtime.onInstalled.addListener((() => {
     console.log('hello');
 }))
@@ -6,14 +15,14 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
         let currentUrl = new URL(tab.url);
         let domain = currentUrl.hostname;
         console.log(domain);
-
-        /*
+/*
         chrome.notifications.create({
             type: 'basic',
             title: 'Nouvelle URL détectée',
             message: `Domaine : ${domain}`,
             iconUrl: "assets/icons/logo48.png"
         });
-        */
+*/
     });
 });
+console.log(document)
